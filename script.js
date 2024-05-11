2da9ba84fb78a6244f44e30bfea038596e479d57
henrick404 <160244828+henrick404@users.noreply.github.com>
2024-05-11 16:38:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,6 +17,26 @@ const HwBt = document.getElementById("HwBt");
 const H18 = document.getElementById("H18");
 const H18Bt = document.getElementById("H18Bt");
 
+// variaveis addicted to chaos
+const AdTC = document.getElementById("AdTC");
+const AdTCBt = document.getElementById("AdTCBt");
+
+// variaveis addicted to chaos
+const Trust = document.getElementById("Trust");
+const TrustBt = document.getElementById("TrustBt");
+
+// variaveis addicted to chaos
+const Sed = document.getElementById("Sed");
+const SedBt = document.getElementById("SedBt");
+
+// variaveis addicted to chaos
+const Mp = document.getElementById("Mp");
+const MpBt = document.getElementById("MpBt");
+
+// variaveis addicted to chaos
+const Wmr = document.getElementById("Wmr");
+const WmrBt = document.getElementById("WmrBt");
+
 // ---------------------------------------------------
 
 function pausarMusicas() { //pausa todas as musicas
@@ -33,6 +53,21 @@ function pausarMusicas() { //pausa todas as musicas
 
     H18.pause();
     H18Bt.setAttribute("name", "play-circle");
+
+    AdTC.pause();
+    AdTCBt.setAttribute("name", "play-circle");
+    
+    Trust.pause();
+    TrustBt.setAttribute("name", "play-circle");
+
+    Sed.pause();
+    SedBt.setAttribute("name", "play-circle");
+
+    Mp.pause();
+    MpBt.setAttribute("name", "play-circle");
+
+    Wmr.pause();
+    WmrBt.setAttribute("name", "play-circle");
 
     tocando = false;
     pausado = true
@@ -54,7 +89,7 @@ function playToS() { //tocar tornado of souls
     pausado = true;
     if (tocando == true || pausado == false) {
         playPause.setAttribute("name", "pause-circle");
-        playPause.style.color = '#ff0000';
+        playPause.style.color = '#990000';
     }
 }
 
@@ -74,7 +109,7 @@ function playHw() { //tocar holy wars
     pausado = true;
     if (tocando == true || pausado == false) {
         playPause.setAttribute("name", "pause-circle");
-        playPause.style.color = '#ff0000';
+        playPause.style.color = '#990000';
     }
 }
 
@@ -94,8 +129,100 @@ function playH18() { //tocar holy wars
     pausado = true;
     if (tocando == true || pausado == false) {
         playPause.setAttribute("name", "pause-circle");
-        playPause.style.color = '#ff0000';
+        playPause.style.color = '#990000';
     }
 }
 
 // ---------------------------------------------------
+
+function playAdTC() { //tocar addicted to chaos
+    if (pausado == true) {
+        pausarMusicas();
+        AdTC.currentTime = 0;
+        AdTC.play();
+        AdTCBt.setAttribute("name", "pause-circle");
+        tocando = true;
+    } else { //pause
+        AdTC.pause();
+        AdTCBt.setAttribute("name", "play-circle");
+    }
+    pausado = true;
+    if (tocando == true || pausado == false) {
+        playPause.setAttribute("name", "pause-circle");
+        playPause.style.color = '#990000';
+    }
+}
+
+// ---------------------------------------------------
+
+function playTrust() { //tocar tornado of souls
+    if (pausado == true) {
+        pausarMusicas();
+        Trust.currentTime = 0;
+        Trust.play();
+        TrustBt.setAttribute("name", "pause-circle");
+        tocando = true;
+    } else { //pause
+        Trust.pause();
+        TrustBt.setAttribute("name", "play-circle");
+    }
+    pausado = true;
+    if (tocando == true || pausado == false) {
+        playPause.setAttribute("name", "pause-circle");
+        playPause.style.color = '#990000';
+    }
+}
+
+function playSed() { //tocar addicted to chaos
+    if (pausado == true) {
+        pausarMusicas();
+        Sed.currentTime = 0;
+        Sed.play();
+        SedBt.setAttribute("name", "pause-circle");
+        tocando = true;
+    } else { //pause
+        Sed.pause();
+        SedBt.setAttribute("name", "play-circle");
+    }
+    pausado = true;
+    if (tocando == true || pausado == false) {
+        playPause.setAttribute("name", "pause-circle");
+        playPause.style.color = '#990000';
+    }
+}
+
+function playMp() { //tocar addicted to chaos
+    if (pausado == true) {
+        pausarMusicas();
+        Mp.currentTime = 0;
+        Mp.play();
+        MpBt.setAttribute("name", "pause-circle");
+        tocando = true;
+    } else { //pause
+        Mp.pause();
+        MpBt.setAttribute("name", "play-circle");
+    }
+    pausado = true;
+    if (tocando == true || pausado == false) {
+        playPause.setAttribute("name", "pause-circle");
+        playPause.style.color = '#990000';
+    }
+}
+
+function playWmr() { //tocar addicted to chaos
+    if (pausado == true) {
+        pausarMusicas();
+        Wmr.currentTime = 0;
+        Wmr.play();
+        WmrBt.setAttribute("name", "pause-circle");
+        tocando = true;
+    } else { //pause
+        Wmr.pause();
+        WmrBt.setAttribute("name", "play-circle");
+    }
+    pausado = true;
+    if (tocando == true || pausado == false) {
+        playPause.setAttribute("name", "pause-circle");
+        playPause.style.color = '#990000';
+    }
+}
